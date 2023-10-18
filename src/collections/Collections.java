f19e8dc2dfd28be6e1a4c33d19ecc33f170fe946
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-10-18 14:07:58 +0100

    ArrayList Method examples

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -4,6 +4,9 @@
  */
 package collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  *
  * @author Lecturer
@@ -17,14 +20,67 @@ public class Collections {
         
         Rectangle[] rectangles = {new Rectangle(2, 3), new Rectangle(4, 1)};
         for (int rectangle = 0; rectangle < rectangles.length; rectangle++) {
-            System.out.println(rectangles[rectangle].length);
+//            System.out.println(rectangles[rectangle].length);
         }    
         
         for (Rectangle rectangle : rectangles) {
             System.out.println(rectangle.width);
         }
         
+        ArrayList<Integer> numList = new ArrayList<>();
         
+        numList.add(5);
+        // {5}
+        numList.add(10);
+        // {5, 10}
+        
+        numList.add(0, 3);
+        // {3, 5, 10}
+        
+        numList.clear();
+        // {}
+        
+        numList.add(5);
+        // {5}
+        
+        numList.contains(4);
+        // false
+        
+        numList.set(0, 10);
+        // {10}
+        
+        numList.add(5);
+        // {10, 5}
+        
+        numList.set(0, 7);
+        // {7, 5}
+        
+        numList.set(1, 2);
+        // {7, 2}
+        
+        ArrayList<String> stringList = new ArrayList<>();
+        
+        stringList.add("Hello!");
+        stringList.add("Hello!");
+        stringList.add("World");
+        
+        // {"Hello!", "Hello!", "World"}
+        
+        stringList.set(0, "World");
+        // {"World", "Hello!" , "World"}
+        
+        stringList.remove("World");
+        // {"Hello!", "World"}
+        
+        // Remove all instances of "World
+//        while (stringList.contains("World")) {
+//            stringList.remove("World");
+//        }
+
+        stringList.remove(1);
+        // {"Hello!"}
+        
+        HashSet<Integer> numSet = new HashSet<>();
     }
     
 
