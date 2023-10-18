8fbc0831aee05a3db49e4dca3ce8295456d39904
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2023-10-18 13:31:16 +0100

    Made a Rectangle class and array with 2 rectangles

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -14,7 +14,7 @@ public class Collections {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Rectangle[] rectangles = {new Rectangle(2, 3), new Rectangle(4, 1)};
     }
     
 }
---
+++ src/collections/Rectangle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package collections;
+
+/**
+ *
+ * @author Lecturer
+ */
+public class Rectangle {
+    int length;
+    int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+    
+    
+}
